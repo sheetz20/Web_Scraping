d19dc7bc32f29ac2480ff4c14c01365f531c7200
sheetal <sheetalshirse20@gmail.com>
2021-03-29 21:59:20 +0530

    movieswithRating

--- ImdbRating.py
+++ ImdbRating.py
@@ -8,20 +8,24 @@ page = requests.get(url)
 
 soup = BeautifulSoup(page.content,'lxml')
 
-csv_file = open("C://Users/sheetal/Desktop/python_projects/rating.csv",'w')
+csv_file = open("C://Users/sheetal/Desktop/WebScrapingProjects/rating.csv",'w')
 csv_writer = csv.writer(csv_file)
 csv_writer.writerow(['MovieName','Rating'])
 # print(soup.prettify())
 
+movieList = []
+ratingList = []
 
 for movieTitle in soup.find_all('td',class_="titleColumn"):
     movieName = movieTitle.a.text
-    print(movieName)
-    csv_writer.writerow([movieName])
+    movieList.append(movieName)
 
 for rating in soup.find_all('td',class_="ratingColumn imdbRating"):
     movieRating = rating.text.strip()
-    print(movieRating)
-    # csv_writer.writerow([movieRating])
+    ratingList.append(movieRating)
+
+for index, name in enumerate(movieList):
+    rating = ratingList[index]
+    csv_writer.writerow([name,rating])
 
 csv_file.close()
